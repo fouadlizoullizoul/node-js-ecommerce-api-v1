a093d263900397b06eabb7a900eb0efc45584199
fouadlizoullizoul <fouadlizoul22@gmail.com>
2024-12-23 10:17:48 +0100

    Categories Models

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -2,8 +2,21 @@ const mongoose = require("mongoose");
 
 //Schema
 const categotySchema = new mongoose.Schema({
-    name:String,
-})
+    name:{
+        type: String,
+        required: [true, 'Category name is required'],
+        unique: [true, 'Category name must be unique'],
+        trim: true,
+        maxlength : [32, 'Category name must be less than 32 characters'],
+        minlength : [3, 'Category name must be more than 3 characters'],
+    },
+    slug: {
+        type: String,
+        lowercase: true,
+    }
+},
+{timestamps: true}
+);
 //Model
 const Category = mongoose.model('Category',categotySchema);
 
