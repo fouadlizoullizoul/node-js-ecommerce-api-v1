00671e019c7b094add1a54989d06bac0e0a8f148
fouadlizoullizoul <fouadlizoul22@gmail.com>
2024-12-23 09:01:58 +0100

    Mongoose Setup

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-app.use(express.json())
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
+//databse connection
 
+mongoose
+  .connect(process.env.DB_URI)
+  .then((conn) => console.log("DB connection successful :",conn.connection.host))
+  .catch((err) => console.log("Database Error",err));
 
+app.use(express.json());
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode ${process.env.NODE_ENV}`);
+}
 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the application" });
+});
 
-app.listen(3000,()=>{
-    console.log('server started on port 3000')
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("server started on port ", PORT);
+});
