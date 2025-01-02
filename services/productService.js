36b505885dd703f47c4e0fea5c3dcc9f5917d8f7
fouadlizoullizoul <fouadlizoul22@gmail.com>
2025-01-02 12:01:45 +0100

    Filtration

--- services/productService.js
+++ services/productService.js
@@ -7,21 +7,29 @@ const ApiError = require("../utils/ApiError");
 //@route GET /api/v1/products
 //@access Public
 exports.getProducts = asyncHandler(async (req, res) => {
+  //1)Filtering
+  const queryStringObj={...req.query}
+  const excludesFields=['page','limit','sort','fields'];
+  excludesFields.forEach(field=>delete queryStringObj[field]);
+  //Apply advanced filtering using [gte,gt,lte,lt]
+  let queryStr=JSON.stringify(queryStringObj);
+  queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match=>`$${match}`);
+
+  //2)Pagination
   const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 5;
+  const limit = req.query.limit * 1 || 50;
   const skip = (page - 1) * limit;
-  const products = await Product.find({})
-    .skip(skip)
-    .limit(limit)
-    .populate({ path: "category", select: "name -_id" });
-  res
-    .status(200)
-    .json({
-      status: "success",
-      results: products.length,
-      page,
-      data: products,
-    });
+
+
+  //Build query
+  const mongooseQuery =Product.find(JSON.parse(queryStr))
+  .skip(skip)
+  .limit(limit)
+  .populate({ path: "category", select: "name -_id" });
+
+  //Execute query
+  const products = await mongooseQuery
+  res.status(200).json({status: "success",results: products.length,page,data: products,});
 });
 //@desc Get Product by ID
 //@route GET /api/v1/products/:id
