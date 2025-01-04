dfceec7527c81cc9fd1b7c04936489329e88517c
fouadlizoullizoul <fouadlizoul22@gmail.com>
2025-01-04 12:46:54 +0100

    Api Features Refactor

--- services/productService.js
+++ services/productService.js
@@ -3,58 +3,24 @@ const asyncHandler = require("express-async-handler");
 const mongoose = require("mongoose");
 const Product = require("../models/productModel");
 const ApiError = require("../utils/ApiError");
+const ApiFeatures = require("../utils/ApiFeatures");
 //@desc Get all Products
 //@route GET /api/v1/products
 //@access Public
 exports.getProducts = asyncHandler(async (req, res) => {
-  //1)Filtering
-  const queryStringObj={...req.query}
-  const excludesFields=['page','limit','sort','fields'];
-  excludesFields.forEach(field=>delete queryStringObj[field]);
-  //Apply advanced filtering using [gte,gt,lte,lt]
-  let queryStr=JSON.stringify(queryStringObj);
-  queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match=>`$${match}`);
-
-  //2)Pagination
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 50;
-  const skip = (page - 1) * limit;
-
 
   //Build query
-  let mongooseQuery =Product.find(JSON.parse(queryStr))
-  .skip(skip)
-  .limit(limit)
-  .populate({ path: "category", select: "name -_id" });
-
-  //3)Sorting
-  if(req.query.sort){
-    const sortBy=req.query.sort.split(',').join(' ')
-    mongooseQuery=mongooseQuery.sort(sortBy)
-  }else{
-    mongooseQuery=mongooseQuery.sort('-createdAt')
-  }
-  //4) Fields Limiting
-  if(req.query.fields){
-    const fields=req.query.fields.split(',').join(' ')
-    mongooseQuery=mongooseQuery.select(fields)
-  }else{
-    mongooseQuery=mongooseQuery.select('-__v')
-  }
-  //5)Seachring
-  const keyword = req.query.keyword?.trim()
-  if(keyword){
-    const query = {
-      $or: [
-          { title: { $regex: keyword, $options: "i" } },
-          { description: { $regex: keyword, $options: "i" } }
-      ]
-  }
-    mongooseQuery=mongooseQuery.find(query)
-  }
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .search()
+    .paginate();
   //Execute query
-  const products = await mongooseQuery
-  res.status(200).json({status: "success",results: products.length,page,data: products,});
+  const products = await apiFeatures.mongooseQuery;
+  res
+    .status(200)
+    .json({ status: "success", results: products.length, data: products });
 });
 //@desc Get Product by ID
 //@route GET /api/v1/products/:id
@@ -64,7 +30,10 @@ exports.getProduct = asyncHandler(async (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return next(new ApiError(404, `Product not found with id ${id}`));
   }
-  const product = await Product.findById(id).populate({path:'category',select:'name -_id'});
+  const product = await Product.findById(id).populate({
+    path: "category",
+    select: "name -_id",
+  });
   if (!product) {
     return next(new ApiError(404, `Product not found with id ${id}`));
   }
@@ -85,8 +54,8 @@ exports.createProduct = asyncHandler(async (req, res) => {
 //@access Private
 exports.updateProduct = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
-  if(req.body.title){
-    req.body.slug = slugify(req.body.title)
+  if (req.body.title) {
+    req.body.slug = slugify(req.body.title);
   }
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return next(new ApiError(400, `Product not found with id ${id}`));
